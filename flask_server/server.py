90ab42b630ab3186264b60da2d18088fbefb7911
dval <dvalentin@middlebury.edu>
2018-12-09 17:32:54 -0500

    Updated server.py

--- flask_server/server.py
+++ flask_server/server.py
@@ -40,24 +40,32 @@ def get_student_info(first_name, last_name):
 
 @app.route('/upload-image/', methods=['POST'])
 def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit a empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-            filename = secure_filename(file.filename)
-            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            data = {
-                success : True
-            }
-            return Response(json.dumps(data), status=200, mimetype='application/json')
+    try:
+        if request.method == 'POST':
+            print("The method is a post request.")
+            # check if the post request has the file part
+            if 'file' not in request.files:
+                flash('No file part')
+                return redirect(request.url)
+            file = request.files['file']
+            print("File: ", file)
+            # if user does not select file, browser also
+            # submit a empty part without filename
+            if file.filename == '':
+                flash('No selected file')
+                print("No file selected!")
+                return redirect(request.url)
+            if file and allowed_file(file.filename):
+                print("We have a file!")
+
+                filename = secure_filename(file.filename)
+                file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+                data = {
+                    success : True
+                }
+                return Response(json.dumps(data), status=200, mimetype='application/json')
+    except Exception as e:
+        print("It didn't work :()")
 
 if __name__ == "__main__":
     app.debug = False
